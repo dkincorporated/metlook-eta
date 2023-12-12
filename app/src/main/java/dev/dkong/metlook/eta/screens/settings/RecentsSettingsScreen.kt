1951e2bc2ac1892c4dcada28acc47ca0ad122354
David Kong <dkon0020@student.monash.edu>
2023-12-12 15:25:39 +1100

    Add sliders to Recents settings screen
    The values are not yet saved anywhere.

--- app/src/main/java/dev/dkong/metlook/eta/screens/settings/RecentsSettingsScreen.kt
+++ app/src/main/java/dev/dkong/metlook/eta/screens/settings/RecentsSettingsScreen.kt
@@ -1,21 +1,41 @@
 package dev.dkong.metlook.eta.screens.settings
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import dev.dkong.metlook.eta.composables.LargeTopAppbarScaffold
+import dev.dkong.metlook.eta.composables.SectionHeading
+import kotlin.math.roundToInt
+
+/**
+ * Enumeration of the types of Recent items
+ * @param qtyPreferenceKey preference key for number of recent items to be saved
+ */
+enum class RecentsType(val qtyPreferenceKey: String) {
+    Stop("recent_stop_count"),
+    Service("recent_service_count")
+}
 
 /**
  * Unified settings screen for recent stops and stations, and recent services
@@ -51,5 +71,46 @@ fun RecentsSettingsScreen(navHostController: NavHostController) {
                 }
             }
         }
+        item {
+            var sliderPosition by remember { mutableFloatStateOf(0f) }
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(
+                    text = "Max number of items",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Text(
+                    text =
+                        if (sliderPosition == 0f) "Disabled"
+                        else sliderPosition.roundToInt().toString(),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Slider(
+                value = sliderPosition,
+                onValueChange = { v -> sliderPosition = v },
+                valueRange = 0f..10f,
+                onValueChangeFinished = {
+                    // TODO: Update preferences
+                },
+                steps = 9,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
     }
+}
+
+/**
+ * Preview
+ */
+@Preview
+@Composable
+fun PreviewRecentsSettings() {
+    RecentsSettingsScreen(navHostController = rememberNavController())
 }
