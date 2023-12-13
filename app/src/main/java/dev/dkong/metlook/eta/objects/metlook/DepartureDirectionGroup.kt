45af50a84f61a4715fa612b7a056749996e81689
David Kong <dkon0020@student.monash.edu>
2023-12-13 16:19:17 +1100

    Make DepartureGroup a Comparable object

--- app/src/main/java/dev/dkong/metlook/eta/objects/metlook/DepartureDirectionGroup.kt
+++ app/src/main/java/dev/dkong/metlook/eta/objects/metlook/DepartureDirectionGroup.kt
@@ -1,5 +1,7 @@
 package dev.dkong.metlook.eta.objects.metlook
 
+import androidx.core.text.isDigitsOnly
+
 /**
  * Record for a group of departures on the Direction level
  */
@@ -7,4 +9,19 @@ data class DepartureDirectionGroup(
     val groupingId: Int,
     val routeNumber: String?,
     val name: String
-)
+) : Comparable<DepartureDirectionGroup> {
+    /**
+     * Comparison function
+     */
+    override fun compareTo(other: DepartureDirectionGroup): Int = compareValuesBy(
+        this,
+        other
+    ) { group ->
+        if (group.routeNumber?.isDigitsOnly() == true) {
+            group.routeNumber.toInt()
+        } else {
+            group.groupingId
+        }
+    }
+
+}
