d664aa27eab65eb706566d95e166dd18155de4b0
David Kong <dkon0020@student.monash.edu>
2024-01-02 16:52:50 +1100

    Change Settings screen to use ListItem
    The leading-content alignment issue will be solved later using a custom implementation.

--- app/src/main/java/dev/dkong/metlook/eta/screens/settings/SettingsScreen.kt
+++ app/src/main/java/dev/dkong/metlook/eta/screens/settings/SettingsScreen.kt
@@ -1,13 +1,31 @@
 package dev.dkong.metlook.eta.screens.settings
 
 import android.app.Activity
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import dev.dkong.metlook.eta.R
+import dev.dkong.metlook.eta.common.ListPosition
 import dev.dkong.metlook.eta.common.Utils.finishActivity
 import dev.dkong.metlook.eta.common.tracker.TrackerManager
 import dev.dkong.metlook.eta.composables.LargeTopAppbarScaffold
@@ -22,6 +40,35 @@ import dev.dkong.metlook.eta.composables.SettingsItem
 fun SettingsScreen(navHostController: NavHostController) {
     val context = LocalContext.current
 
+    // Define the settings pages
+    /**
+     * Record for a settings page
+     */
+    data class SettingsPage(
+        val title: String,
+        val subtitle: String? = null,
+        @DrawableRes val icon: Int,
+        val onClick: () -> Unit
+    )
+
+    val pages = arrayOf(
+        SettingsPage(
+            "Location-based features",
+            "Decide whether and how your location is used",
+            R.drawable.outline_location_on_24
+        ) { navHostController.navigate(SettingsScreens.LocationFeatures.route) },
+        SettingsPage(
+            "Dashboard recents",
+            "Save recent stops, stations and services",
+            R.drawable.baseline_history_24
+        ) { navHostController.navigate(SettingsScreens.Recents.route) },
+        SettingsPage(
+            TrackerManager.integrationSettingsName,
+            TrackerManager.integrationSettingsDescription,
+            R.drawable.outline_dataset_linked_24
+        ) { navHostController.navigate(SettingsScreens.TrackerIntegration.route) }
+    )
+
     LargeTopAppbarScaffold(
         navController = navHostController,
         title = "Settings",
@@ -30,34 +77,49 @@ fun SettingsScreen(navHostController: NavHostController) {
             context.finishActivity()
         }
     ) {
-        item {
-            MaterialColumn {
-                SettingsItem(
-                    name = "Location-based features",
-                    description = "Decide whether/how your location is used",
-                    onClick = {
-                        navHostController.navigate(SettingsScreens.LocationFeatures.route)
+        pages.forEachIndexed { index, page ->
+            item {
+                ListItem(
+                    headlineContent = {
+                        Text(
+                            text = page.title,
+                            style = MaterialTheme.typography.titleLarge
+                        )
                     },
-                    iconHasCircle = true,
-                    icon = R.drawable.outline_location_on_24
-                )
-                SettingsItem(
-                    name = "Dashboard recents",
-                    description = "Save recent stops, stations and services",
-                    onClick = {
-                        navHostController.navigate(SettingsScreens.Recents.route)
+                    supportingContent = {
+                        page.subtitle?.let {
+                            Text(
+                                text = it,
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
                     },
-                    iconHasCircle = true,
-                    icon = R.drawable.baseline_history_24
-                )
-                SettingsItem(
-                    name = TrackerManager.integrationSettingsName,
-                    description = TrackerManager.integrationSettingsDescription,
-                    onClick = {
-                        navHostController.navigate(SettingsScreens.TrackerIntegration.route)
+                    leadingContent = {
+                        Box(
+                            modifier =
+                            Modifier
+                                .size(40.dp)
+                                .clip(CircleShape)
+                                .background(MaterialTheme.colorScheme.primaryContainer)
+                        ) {
+                            Image(
+                                painter = painterResource(id = page.icon),
+                                contentDescription = null,
+                                colorFilter = ColorFilter.tint(
+                                    MaterialTheme.colorScheme.onPrimaryContainer
+                                ),
+                                modifier = Modifier
+                                    .size(32.dp)
+                                    .padding(4.dp)
+                                    .align(Alignment.Center)
+                            )
+                        }
                     },
-                    iconHasCircle = true,
-                    icon = R.drawable.outline_dataset_linked_24
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 1.dp)
+                        .clip(ListPosition.fromPosition(index, pages.size).roundedShape)
+                        .background(MaterialTheme.colorScheme.surface)
+                        .clickable { page.onClick() }
                 )
             }
         }
